2abe3197cf9b2efee33e4da273b03beda6a1ea6b
kouroshsalahshoor <kourosh23@hotmail.com>
2022-07-10 16:43:49 +0200

    Add project files.

---
+++ SportsStore/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public long? ProductID { get; set; }
+        [Required(ErrorMessage = "Please enter a product {0}")]
+        public string Name { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Please enter a {0}")]
+        public string Description { get; set; } = String.Empty;
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive {0}")]
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Please specify a {0}")]
+        public string Category { get; set; } = String.Empty;
+    }
+}
---
+++ SportsStore/Models/StoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class StoreDbContext : IdentityDbContext<IdentityUser>
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+        : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Order> Orders => Set<Order>();
+
+    }
+}
